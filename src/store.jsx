56fc7c16eee1c10445645c9288a3fb5629a27e6f
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2025-04-04 14:34:28 +0300

    dispatched some actions

--- src/store.jsx
+++ src/store.jsx
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -11,9 +13,14 @@ function reducer(state = initialState, action) {
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
-      if (state.loan === 0) return state;
+      if (state.loan > 0) return state;
       //Later
-      return { ...state, load: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -25,3 +32,21 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
+store.dispatch({
+  type: "account/requestLoan",
+  payload: {
+    amount: 1000,
+    purpose: "Buy a car",
+  },
+});
+
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
