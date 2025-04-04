6c0be1374810e2bf3277a7d63a7c1a1a15ff884e
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2025-04-04 22:07:24 +0300

    created a new & organized folder structure. Also brought store to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import "./store.jsx";
+import store from "./store.jsx";
+
+store.dispatch({ type: "account/deposit", payload: 250 });
+console.log(store.getState());
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
