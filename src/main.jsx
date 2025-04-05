98228caaba2cb5d2b865b8be869234e48fe2cf06
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2025-04-05 09:26:09 +0300

    added react-redux library and connected it to react application

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 
 import store from "./store.jsx";
 
-store.dispatch({ type: "account/deposit", payload: 250 });
-console.log(store.getState());
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
